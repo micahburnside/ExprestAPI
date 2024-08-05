b4b2bb3b1ea8e0ce1188af4478777cff0c015bc0
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 17:33:38 -0400

    Use environment variable for MongoDB connection URL

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/subscribers', {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
