2680dcd970bbcef7533741817b3cb40b63c17464
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 18:34:32 -0400

    Add 'required' constraint to subscriber name

--- models/subscriber.js
+++ models/subscriber.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const subscriberSchema = new.mongoose.Schema({
   name: {
-    type: String
+    type: String,
+    required: true
   },
   subscribeToChannel: {
 
