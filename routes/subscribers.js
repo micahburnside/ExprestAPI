88a7a7d02f40bd5a0d4bfcb538c75bdb120ced5c
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 18:17:19 -0400

    Add basic route structure in subscribers.js

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,19 @@
 const express = require('express')
+const {request} = require("express");
 const router = express.Router()
+
+
+// Getting All
+router.get('/:id', (req, res) => {
+
+})
+
+// Getting One
+
+// Creating One
+
+// Updating One
+
+// Deleting One
 
 module.exports = router
