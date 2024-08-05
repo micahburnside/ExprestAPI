1b7c43ea7220d6dd9090a8c4cef5d84eb7c9a8e2
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 19:35:36 -0400

    Update router post endpoint and implement new subscriber creation

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -18,8 +18,17 @@ res.send(req.params.id)
 })
 
 // Creating One
-router.post('/:id', async (req, res) => {
-
+router.post('/', async (req, res) => {
+const subscriber = new Subscriber({
+  name: req.body.name,
+  subscribedToChannel: req.body.subscribedToChannel
+})
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 
 // Updating One
