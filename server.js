ff567eebaa6cedb6e4b8ed06f7dd71807037b0b4
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 17:29:01 -0400

    Add MongoDB connection setup

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/subscribers', {useNewUrlParser: true})
 
 app.listen(3000, () => console.log('Server Started'))
