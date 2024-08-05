94522176cd9f5cb10947ab19277161854ab7d9b6
Micah Burnside <micahburnside@protonmail.com>
2024-08-05 17:34:44 -0400

    Add JSON parsing middleware

--- server.js
+++ server.js
@@ -9,4 +9,7 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
+//allows server to accept JSON
+app.use(express.json())
+
 app.listen(3000, () => console.log('Server Started'))
